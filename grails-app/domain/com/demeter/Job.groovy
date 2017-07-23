4429ae222080627acea2f99f65baa26f4e2cbd0a
Darko Rajin <drajin@tecracer.de>
2017-07-23 16:38:21 +0200

    Making job list get all the properties

--- grails-app/domain/com/demeter/Job.groovy
+++ grails-app/domain/com/demeter/Job.groovy
@@ -7,25 +7,21 @@ import grails.rest.Resource
 class Job {
 
     Date startDate
-    Integer workHours
     String description
-    String code
+    JobType jobType
     Farmland farmland
-    static hasMany = [responsible: Person,
-                      machines   : Machine]
+    Person responsible
+    static hasMany = [machines: Machine]
+
+    static mapping = {
+        machines lazy: false
+    }
 
     static constraints = {
         startDate nullable: true
-        workHours nullable: true
-        description nullable: false, blank: false
+        description nullable: true
         farmland nullable: false, blank: false
-        code nullable: true, blank: true
-        responsible validator: { val, obj ->
-            def retval = true
-            if (!obj?.responsible?.size()) {
-                retval = 'job.validator.hasnoresponsible.error'
-            }
-            return retval
-        }
+        jobType nullable: true
+        responsible nullable: false
     }
 }
--- grails-app/init/demeter/api/BootStrap.groovy
+++ grails-app/init/demeter/api/BootStrap.groovy
@@ -2,21 +2,43 @@ package demeter.api
 
 import com.demeter.Farmland
 import com.demeter.Job
+import com.demeter.JobType
 import com.demeter.Machine
 import com.demeter.Person
 
+
 class BootStrap {
 
+
     def init = { servletContext ->
+        new JobType(name: 'Spremanje').save()
+        new JobType(name: 'Oranje').save()
+        new JobType(name: 'Prskanje').save()
+        new JobType(name: 'Djubrenje').save()
+        new JobType(name: 'Zalivanje').save()
+
         new Machine(make: "John Deer", model: "1200s", vin: "1234").save()
         new Machine(make: "John Deer", model: "1500rc", vin: "2234").save()
         new Farmland(name: "mirkova njiva", size: 43, number: 234554, location: "iza hetina").save()
         new Farmland(name: "Jovina zemlja", size: 98, number: 65453, location: "prema itebeju").save()
-        def per = new Person(firstname: "Darko", lastname: "Rajin").save()
+        new Person(firstname: "Kristian", lastname: "Kum", email: "some@mail.com", password: "123").save()
 
         new Job(
-                description: "oranje", responsible: [Person.get(1)]
+                description: "sredio sam kako valja",
+                responsible: Person.get(1),
+                jobType: JobType.get(2),
+                farmland: Farmland.get(1),
+                machines: [Machine.get(1)]
         ).save()
+
+        new Job(
+                description: "Zalio sve",
+                responsible: Person.get(1),
+                jobType: JobType.get(5),
+                farmland: Farmland.get(2),
+                machines: [Machine.get(2)]
+        ).save()
+
     }
 
     def destroy = {
